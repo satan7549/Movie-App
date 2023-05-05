24dc61a8d895c49245e1e752ede30bf75d8c727f
Satan Sharma <satansharma7549@gmail.com>
2023-05-05 21:52:09 +0530

    Create Movies.jsx

---
+++ movie-app/src/Pages/Movies.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import MovieList from "../components/MovieList";
+// import SideBar from "../components/SideBar";
+
+const Movies = () => {
+  return <MovieList />;
+};
+
+export default Movies;
